1e1e6265912c42f2fe279bcfb04cc2031959dbe2
Anurag Rachcha <rachcha.anurag13@gmail.com>
2020-02-11 02:07:30 -0500

    Assignment 1
    Taking inputs and displaying Car related information along with images in Java Swing

---
+++ Assignment1/src/car/Attributes.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car;
+
+import javax.swing.JLabel;
+
+
+/**
+ *
+ * @author Admin
+ */
+public class Attributes {
+    
+    private String Brand;
+    private String Model;
+    private String Color;
+    private String CarClass;
+    private String PassNumber;
+    private String DriveType;
+    private String TyreType;
+    private String Transmission;
+    private String EngineType;
+    private String AirbagNo;
+    private String GasType;
+    private String PaintType;
+    private String Mileage;
+    private String FuelCapacity;
+    private String Price;
+    private JLabel jlabel99;
+
+
+    public String getBrand() {
+        return Brand;
+    }
+
+    public void setBrand(String Brand) {
+        this.Brand = Brand;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String Model) {
+        this.Model = Model;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String Color) {
+        this.Color = Color;
+    }
+
+    public String getCarClass() {
+        return CarClass;
+    }
+
+    public void setCarClass(String CarClass) {
+        this.CarClass = CarClass;
+    }
+
+    public String getPassNumber() {
+        return PassNumber;
+    }
+
+    public void setPassNumber(String PassNumber) {
+        this.PassNumber = PassNumber;
+    }
+
+    public String getDriveType() {
+        return DriveType;
+    }
+
+    public void setDriveType(String DriveType) {
+        this.DriveType = DriveType;
+    }
+
+    public String getTyreType() {
+        return TyreType;
+    }
+
+    public void setTyreType(String TyreType) {
+        this.TyreType = TyreType;
+    }
+
+    public String getTransmission() {
+        return Transmission;
+    }
+
+    public void setTransmission(String Transmission) {
+        this.Transmission = Transmission;
+    }
+
+    public String getEngineType() {
+        return EngineType;
+    }
+
+    public void setEngineType(String EngineType) {
+        this.EngineType = EngineType;
+    }
+
+    public String getAirbagNo() {
+        return AirbagNo;
+    }
+
+    public void setAirbagNo(String AirbagNo) {
+        this.AirbagNo = AirbagNo;
+    }
+
+    public String getGasType() {
+        return GasType;
+    }
+
+    public void setGasType(String GasType) {
+        this.GasType = GasType;
+    }
+
+    public String getPaintType() {
+        return PaintType;
+    }
+
+    public void setPaintType(String PaintType) {
+        this.PaintType = PaintType;
+    }
+
+    public String getMileage() {
+        return Mileage;
+    }
+
+    public void setMileage(String Mileage) {
+        this.Mileage = Mileage;
+    }
+
+    public String getFuelCapacity() {
+        return FuelCapacity;
+    }
+
+    public void setFuelCapacity(String FuelCapacity) {
+        this.FuelCapacity = FuelCapacity;
+    }
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public void setPrice(String Price) {
+        this.Price = Price;
+    }
+    
+        public JLabel getJlabel99() {
+        return jlabel99;
+    }
+
+    public void setJlabel99(JLabel label) {
+        this.jlabel99 = label;
+    }
+
+    
+
+
+
+    // public void setimage(File imagename) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //}
+    
+    
+    
+}
